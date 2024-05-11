02b274d20c7e2b64383ffc8c9a721cadefa4e34e
NoyLeibo <150171476+NoyLeibo@users.noreply.github.com>
2024-05-11 13:22:26 +0300

    add comments to fetchVideos

--- services/fetchVideos.js
+++ services/fetchVideos.js
@@ -10,7 +10,7 @@ export const fetchVideos = async (query = 'latest videos') => {
     if (query === 'latest videos') {
         let data = await _getData(VIDEOS_KEY)
         if (data) return data
-    }
+    } // to not fetch new videos every refresh (reason: youtube blocked me for a lot of requests)
     try {
         const response = await axios.get(`${BASE_URL}/search`, {
             params: {
@@ -21,33 +21,32 @@ export const fetchVideos = async (query = 'latest videos') => {
                 q: query
             }
         });
-        console.log('API Response:', response.data); // this line is to log the response
+        // console.log('API Response:', response.data) // this line is to log the response
         if (query === 'latest videos') _storeData(VIDEOS_KEY, response.data.items)
-        return response.data.items;
+        return response.data.items
     } catch (error) {
-        console.error("Error fetching videos:", error);
+        console.error("Error fetching videos:", error)
         throw error;
     }
 };
 
-//Using storeData and getData to stop block from youtbe
-
-const _storeData = async (key, value) => {
+// Using storeData and getData to stop block from youtbe
+async function _storeData(key, value) {
     try {
-        await AsyncStorage.setItem(key, JSON.stringify(value));
+        await AsyncStorage.setItem(key, JSON.stringify(value))
     } catch (error) {
-        console.error("error:", error);
+        console.error("error:", error)
     }
 };
 
-const _getData = async (key) => {
+async function _getData(key) {
     try {
-        const value = await AsyncStorage.getItem(key);
+        const value = await AsyncStorage.getItem(key)
         if (value !== null) {
-            return JSON.parse(value);
+            return JSON.parse(value)
         }
-        throw new Error("empty cache");
+        throw new Error("empty cache")
     } catch (error) {
-        console.error("error:", error);
+        console.error("error:", error)
     }
 };
